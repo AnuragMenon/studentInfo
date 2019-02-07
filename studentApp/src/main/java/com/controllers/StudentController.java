ab0dbab2725cb6290314b2cae23f43c255685b52
AnuragMenon <anurag.menon16@gmail.com>
2019-02-07 17:48:03 +0530

    Works fine now

--- studentApp/src/main/java/com/controllers/StudentController.java
+++ studentApp/src/main/java/com/controllers/StudentController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import java.util.Map;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -12,12 +13,12 @@ import org.springframework.web.servlet.ModelAndView;
 
 import com.entities.Student;
 import com.services.StudentServices;
-import com.servicesImpl.StudentServicesImpl;
 
 @Controller
 @RequestMapping(value = "users")
 public class StudentController {
 
+	@Autowired
 	StudentServices userServices;
 
 	@RequestMapping(value = "/page", method = RequestMethod.GET)
--- studentApp/src/main/java/com/daoImpl/StudentDaoImpl.java
+++ studentApp/src/main/java/com/daoImpl/StudentDaoImpl.java
@@ -3,16 +3,17 @@ package com.daoImpl;
 import java.util.List;
 
 import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
-
+import org.springframework.web.bind.annotation.ResponseBody;
 import com.dao.StudentDao;
 import com.entities.Student;
 
 @Repository
 @Transactional
 public class StudentDaoImpl implements StudentDao {
-
+@Autowired
 SessionFactory session;
 	
 	public boolean saveOrUpdate(Student users) {
